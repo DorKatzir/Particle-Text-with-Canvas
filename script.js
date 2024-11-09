24ccf0f8fcd20161a8cb9d9160e355b66090f1c0
Dor Katzir <katzirdor@gmail.com>
2024-11-09 16:53:34 +0200

    Refactoring into object oriented JavaScript

--- script.js
+++ script.js
@@ -1,75 +1,101 @@
 window.addEventListener('load', function() {
 
-    const textInput = document.getElementById('textInput')
     const canvas = document.getElementById('canvas1')
     const ctx = canvas.getContext('2d')
     canvas.width = window.innerWidth // covers all window horizontaly
     canvas.height = window.innerHeight // covers all window vertically
     console.log(ctx)
-
-    ctx.lineWidth = 3
-    ctx.strokeStyle = 'red'
-    ctx.beginPath()
-    ctx.moveTo(canvas.width/2, 0)
-    ctx.lineTo(canvas.width/2, canvas.height)
-    ctx.stroke()
-
-    ctx.strokeStyle = 'green'
-    ctx.beginPath()
-    ctx.moveTo(0, canvas.height/2)
-    ctx.lineTo(canvas.width, canvas.height/2)
-    ctx.stroke()
-
-    const gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height)    
-    gradient.addColorStop(0.3, 'red')
-    gradient.addColorStop(0.5, 'fuchsia')
-    gradient.addColorStop(0.7, 'purple')
-    ctx.fillStyle = gradient
-    ctx.strokeStyle = 'white'
     
-    ctx.font = '80px Trebuchet MS'
-    ctx.textAlign = 'center'
-    ctx.textBaseline = 'middle'
+    class Particle {
+        constructor() {
 
-    const maxTextWidth = canvas.width * 0.8
-    const lineHeight = 80
-
-    function wrapText(text) {
-
-        let linesArray = []
-        let lineCounter = 0
-        let line = ''
-        let words = text.split(' ') // creates array of words from the text variable
-
-        for (let i = 0; i < words.length; i++) {
-
-            let testLine = line + words[i] + ' '
-
-            if (ctx.measureText(testLine).width > maxTextWidth) {
-                line = words[i] + ' '
-                lineCounter++
-            } else {
-                line = testLine
-            }
-
-            linesArray[lineCounter] = line
+        }
+        
+        draw() {
+            
+        }
+        
+        update() {
+            
+        }
+        
+    } // End of Particle class
+    
+    
+    class Effect {
+        constructor(context, canvasWidth, canvasHeight) {
+            this.context = context
+            this.canvasWidth = canvasWidth
+            this.canvasHeight = canvasHeight
+            this.textX = this.canvasWidth / 2
+            this.textY = this.canvasHeight / 2
+            this.fontSize = 100
+            this.lineHeight = this.fontSize * 0.96
+            this.maxTextWidth = this.canvasWidth * 0.8
+            this.textInput = document.getElementById('textInput')
+            this.textInput.addEventListener('keyup', (e) => {
+                if (e.key !== ' ') {
+                    this.context.clearRect(0, 0, this.canvasWidth, this.canvasHeight)
+                    this.wrapText(e.target.value) 
+                }
+            })
         }
 
-        let textHeight = lineHeight * lineCounter
-        let textY = canvas.height/2 - textHeight/2
-        
-        linesArray.forEach((line, i) => {
-            ctx.fillText(line, canvas.width/2, textY + lineHeight * i)
-        })
-        console.log(linesArray)
+        wrapText(text) {
+            //canvas settings
+            const gradient = this.context.createLinearGradient(0, 0, this.canvasWidth, this.canvasHeight)    
+            gradient.addColorStop(0.3, 'red')
+            gradient.addColorStop(0.5, 'fuchsia')
+            gradient.addColorStop(0.7, 'purple')
+            this.context.fillStyle = gradient 
+            this.context.textAlign = 'center'
+            this.context.textBaseline = 'middle'
+            this.context.lineWidth = 3
+            // this.context.strokeStyle = 'rgba(255, 255, 255, 0)'
+            this.context.font = this.fontSize + 'px Trebuchet MS'
+            // breake multiple text
+            let linesArray = []
+            let words = text.split(' ')
+            let lineCounter = 0
+            let line = ''
+            for (let i = 0; i < words.length; i++) {
+                let testLine = line + words[i] + ' '
+                if (this.context.measureText(testLine).width > this.maxTextWidth) {
+                    line = words[i] + ' '
+                    lineCounter++
+                } 
+                else {
+                    line = testLine
+                }
+                linesArray[lineCounter] = line
+            }
+            let textHeight = this.lineHeight * lineCounter
+            let textY = this.canvasHeight/2 - textHeight/2
+            linesArray.forEach((el, index) => {
+                this.context.fillText(el, this.canvasWidth/2, textY + (this.lineHeight * index))
+                // this.context.strokeText(el, this.canvasWidth/2, textY + (this.lineHeight * index))
+            })
+
+        }
+
+        convertToParticles() {
+
+        }
+
+        render() {
+
+        }
+
+    } // End of Effect class
+
+    const effect = new Effect(ctx, canvas.width, canvas.height)
+    effect.wrapText('Hello dror what\'s on your mind today?')
+    console.log(effect)
+
+    function animate() {
+
     }
 
-    // wrapText('Hello Dror and everyone else! This is a test to see how this works. I hope it works!')
 
-    textInput.addEventListener('keyup', function() {
-        console.clear()
-        ctx.clearRect(0, 0, canvas.width, canvas.height)
-        wrapText(textInput.value)
-    })
 
 })
